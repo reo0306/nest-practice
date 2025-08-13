9aab881ac9517cb7fa0561c21c7a312b62f14ab8
reo0306 <reo19840306@gmail.com>
2025-08-13 14:54:08 +0900

    add model, service

--- nestjs-fleamarket/src/items/items.controller.ts
+++ nestjs-fleamarket/src/items/items.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import type { Item } from './items.model';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll() {
+    return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: 'ON_SALE',
+    };
+    return this.itemsService.create(item);
+  }
+}
---
+++ nestjs-fleamarket/src/items/items.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import type { Item } from './items.model';
+
+@Injectable()
+export class ItemsService {
+  private items: Item[] = [];
+
+  findAll() {
+    return 'This is ItemsService';
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+}
